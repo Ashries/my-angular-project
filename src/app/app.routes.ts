07a1257ffe760494b89987f12e8939c76b04b489
Tommy Nguyen <hieu.nguyen@student.lab.fi>
2025-11-24 10:59:18 +0200

    updated reititys

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { FormComponent } from './form/form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'hello', component: HelloWorldComponent },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'feedback', component: FeedbackComponent },
